45d3c3dbb040f8776801d1f263b5944016604e51
Markus Skov <markusskov@hotmail.no>
2026-04-10 12:04:47 +0200

    Initial Commit

---
+++ Sonoic/SonoicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonoicApp.swift
+//  Sonoic
+//
+//  Created by Markus Skov on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SonoicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
